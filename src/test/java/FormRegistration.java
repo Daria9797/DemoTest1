aa7779653fb28c2db8222e437960cd7e15a47a70
Daria9797 <144155239+Daria9797@users.noreply.github.com>
2023-10-30 20:29:49 +0300

    Update FormRegistration.java

--- src/test/java/FormRegistration.java
+++ src/test/java/FormRegistration.java
@@ -1,51 +1,13 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import helpers.Attach;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.util.Map;
-
+import static io.qameta.allure.Allure.step;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
 @Tag("demoqa")
-public class FormRegistration {
-    @BeforeAll
-    static void beforeAll()
-    {
-        Configuration.baseUrl="https://demoqa.com";
-        Configuration.pageLoadStrategy="eager";
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("version", "100");
-        Configuration.browserSize = System.getProperty("size", "1920x1080");
-        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options",
-                Map.<String, Object> of(
-                        "enableVNC", true,
-                        "enableVideo", true
-                ));
-        Configuration.browserCapabilities = capabilities;
+public class FormRegistration extends TestBase{
 
-    }
-    @BeforeEach
-    void addListener() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    }
-
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        if (!Configuration.browser.equalsIgnoreCase("firefox")){
-            Attach.browserConsoleLogs();
-        }
-        Attach.addVideo();
-    }
     @Test
     void enterFormTest()
     {
